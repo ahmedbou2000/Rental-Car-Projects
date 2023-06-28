8d3bc7c6e3b8cf600bc30e6d48e33375fa299e17
bouguerba ahmed <bouguerbaahmed06@gmail.com>
2023-06-28 18:48:02 +0200

    Liste des locations , liste des reservations dans la page des reservations

--- src/main/java/com/rentaloo/rentaloo/ReservationController.java
+++ src/main/java/com/rentaloo/rentaloo/ReservationController.java
@@ -17,6 +17,8 @@ import java.io.File;
 import java.net.URL;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
 
 public class ReservationController implements Initializable {
@@ -35,15 +37,14 @@ public class ReservationController implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        ConfigureRentalListTable();
-        ConfigureReservationListTable();
-        Resize();
+        try {
+            ConfigureRentalListTable();
+            ConfigureReservationListTable();
+        }catch (Exception e){
+            throw new RuntimeException(e);
+        }
+            Resize();
 
-//        try {
-//        } catch (ClassNotFoundException e) {
-//            System.out.println("Connection Failed");
-//            throw new RuntimeException(e);
-//        }
         btnAojuterResevation.setOnAction(event -> {
 
             String nom = "";
@@ -64,57 +65,77 @@ public class ReservationController implements Initializable {
 
 
     // this method is a configuration for rental List Table
-    public void ConfigureRentalListTable() {
+    public void ConfigureRentalListTable() throws Exception {
+        TableColumn<StatsController.Rent, String> numReservationCol = new TableColumn<>("N° Reservation");
+        numReservationCol.setCellValueFactory(new PropertyValueFactory<>("NumReservation"));
+        numReservationCol.prefWidthProperty().bind(ReservationListTable.widthProperty().divide(5));
+
+
         TableColumn<StatsController.Rent, String> immatriculeCol = new TableColumn<>("Immatriculation");
         immatriculeCol.setCellValueFactory(new PropertyValueFactory<>("Immatriculation"));
-        immatriculeCol.prefWidthProperty().bind(RentalListTable.widthProperty().divide(4));
+        immatriculeCol.prefWidthProperty().bind(RentalListTable.widthProperty().divide(5));
         immatriculeCol.setMaxWidth(Double.MAX_VALUE);
 
         TableColumn<StatsController.Rent, String> NomCompletCol = new TableColumn<>("NomComplet");
         NomCompletCol.setCellValueFactory(new PropertyValueFactory<>("NomComplet"));
-        NomCompletCol.prefWidthProperty().bind(RentalListTable.widthProperty().divide(4));
+        NomCompletCol.prefWidthProperty().bind(RentalListTable.widthProperty().divide(5));
         NomCompletCol.setMaxWidth(Double.MAX_VALUE);
 
         TableColumn<StatsController.Rent, String> DepartCol = new TableColumn<>("DateDepart");
         DepartCol.setCellValueFactory(new PropertyValueFactory<>("DateDepart"));
-        DepartCol.prefWidthProperty().bind(RentalListTable.widthProperty().divide(4));
+        DepartCol.prefWidthProperty().bind(RentalListTable.widthProperty().divide(5));
         DepartCol.setMaxWidth(Double.MAX_VALUE);
 
         TableColumn<StatsController.Rent, String> RetourCol = new TableColumn<>("DateRetour");
         RetourCol.setCellValueFactory(new PropertyValueFactory<>("DateRetour"));
-        RetourCol.prefWidthProperty().bind(RentalListTable.widthProperty().divide(4));
+        RetourCol.prefWidthProperty().bind(RentalListTable.widthProperty().divide(5));
         RetourCol.setMaxWidth(Double.MAX_VALUE);
 
         RentalListTable.getColumns().clear();
-        RentalListTable.getColumns().addAll(immatriculeCol, NomCompletCol, DepartCol, RetourCol);
+        RentalListTable.getColumns().addAll(numReservationCol,immatriculeCol, NomCompletCol, DepartCol, RetourCol);
+        try {
+            // get data :
+            ResultSet result = DbContext.Execute("SELECT r.idReservation , v.IMMATRICULE , CONCAT(c.NOM, \" \" ,c.PRENOM) as 'NomComplet' , r.DATEDEBUT , r.DATEFIN \n" +
+                    "FROM reservation r\n" +
+                    "INNER JOIN detail d on d.IDRESERVATION = r.IDRESERVATION\n" +
+                    "INNER JOIN voiture v on v.IDVOITURE = r.IDVOITURE \n" +
+                    "INNER JOIN client c on c.IDCLIENT = r.IDCLIENT\n" +
+                    "WHERE d.STATUT = 'comfirmé' " +
+                    "ORDER BY r.idReservation DESC");
 
+            List<StatsController.Rent> listLocations = new ArrayList<>();
+            while (result.next()) {
+                listLocations.add(new StatsController.Rent(
+                        result.getString("idReservation"),
+                        result.getString("immatricule"),
+                        result.getString("NomComplet"),
+                        result.getString("dateDebut"),
+                        result.getString("dateFin")
+                ));
+            }
+                // set the data to the table :
+                RentalListTable.setItems(FXCollections.observableArrayList(listLocations));
 
-        RentalListTable.setItems(FXCollections.observableArrayList(
-                new StatsController.Rent("12345-A-49", "BOUKHRISS MOHAMED", "01/05/2023", "12/05/2023"),
-                new StatsController.Rent("12346-A-49", "BENAISSATI AHMED", "02/05/2023", "21/05/2023"),
-                new StatsController.Rent("12347-A-49", "CHIKOUR KAMEL", "06/05/2023", "30/05/2023"),
-                new StatsController.Rent("12348-A-49", "BELAMIN JAWAD", "02/05/2023", "13/05/2023"),
-                new StatsController.Rent("12345-A-49", "BOUKHRISS MOHAMED", "01/05/2023", "12/05/2023"),
-                new StatsController.Rent("12346-A-49", "BENAISSATI AHMED", "02/05/2023", "21/05/2023"),
-                new StatsController.Rent("12347-A-49", "CHIKOUR KAMEL", "06/05/2023", "30/05/2023"),
-                new StatsController.Rent("12348-A-49", "BELAMIN JAWAD", "02/05/2023", "13/05/2023")
-
-        ));
+        }catch(Exception e){
+            throw new Exception();
+        }
     }
 
     //this method is a  configuration for Reservation List Table
-    public void ConfigureReservationListTable() {
-        TableColumn<ReservationModel, String> immatCol = new TableColumn<>("Immatricule");
-        immatCol.setCellValueFactory(new PropertyValueFactory<>("Immatricule"));
-        immatCol.prefWidthProperty().bind(ReservationListTable.widthProperty().divide(5));
+    public void ConfigureReservationListTable() throws Exception {
+        TableColumn<ReservationModel, String> numReservationCol = new TableColumn<>("N° Reservation");
+        numReservationCol.setCellValueFactory(new PropertyValueFactory<>("NumReservation"));
+        numReservationCol.prefWidthProperty().bind(ReservationListTable.widthProperty().divide(5));
 
-        TableColumn<ReservationModel, String> Nom = new TableColumn<>("Nom");
-        Nom.setCellValueFactory(new PropertyValueFactory<>("Nom"));
-        Nom.prefWidthProperty().bind(ReservationListTable.widthProperty().divide(5));
+        TableColumn<StatsController.Rent, String> immatriculeCol = new TableColumn<>("Immatriculation");
+        immatriculeCol.setCellValueFactory(new PropertyValueFactory<>("Immatriculation"));
+        immatriculeCol.prefWidthProperty().bind(RentalListTable.widthProperty().divide(5));
+        immatriculeCol.setMaxWidth(Double.MAX_VALUE);
 
-        TableColumn<ReservationModel, String> Prenom = new TableColumn<>("Prenom");
-        Prenom.setCellValueFactory(new PropertyValueFactory<>("Prenom"));
-        Prenom.prefWidthProperty().bind(ReservationListTable.widthProperty().divide(5));
+        TableColumn<ReservationModel, String> NomComplet = new TableColumn<>("NomComplet");
+        NomComplet.setCellValueFactory(new PropertyValueFactory<>("NomComplet"));
+        NomComplet.prefWidthProperty().bind(ReservationListTable.widthProperty().divide(5));
+
 
         TableColumn<ReservationModel, String> DateDepart = new TableColumn<>("DateDepart");
         DateDepart.setCellValueFactory(new PropertyValueFactory<>("DateDepart"));
@@ -126,20 +147,47 @@ public class ReservationController implements Initializable {
 
 
         ReservationListTable.getColumns().clear();
-        ReservationListTable.getColumns().addAll(immatCol, Nom, Prenom, DateDepart, DateRetour);
+        ReservationListTable.getColumns().addAll(numReservationCol, immatriculeCol, NomComplet, DateDepart, DateRetour);
 
-        ReservationListTable.setItems(FXCollections.observableArrayList(
-                new ReservationModel(1, "12345-A-49", "BOUKHRISS", "MOHAMED", "01/05/2023", "12/05/2023"),
-                new ReservationModel(2, "12345-A-49", "BOUKHRISS", "MOHAMED", "01/05/2023", "12/05/2023"),
-                new ReservationModel(3, "12345-A-49", "BOUKHRISS", "MOHAMED", "01/05/2023", "12/05/2023")
-        ));
+        try {
+            // get data :
+            ResultSet result = DbContext.Execute("SELECT r.idReservation , v.IMMATRICULE , CONCAT(c.NOM, \" \" ,c.PRENOM) as 'NomComplet' , r.DATEDEBUT , r.DATEFIN \n" +
+                    "FROM reservation r\n" +
+                    "INNER JOIN detail d on d.IDRESERVATION = r.IDRESERVATION\n" +
+                    "INNER JOIN voiture v on v.IDVOITURE = r.IDVOITURE \n" +
+                    "INNER JOIN client c on c.IDCLIENT = r.IDCLIENT\n" +
+                    "WHERE d.STATUT = 'en attente' " +
+                    "ORDER BY r.idReservation DESC");
 
+            List<StatsController.Rent> listReservations = new ArrayList<>();
+            while (result.next()) {
+                listReservations.add(new StatsController.Rent(
+                        result.getString("idReservation"),
+                        result.getString("immatricule"),
+                        result.getString("NomComplet"),
+                        result.getString("dateDebut"),
+                        result.getString("dateFin")
+                ));
+            }
+            // set the data to the table :
+            ReservationListTable.setItems(FXCollections.observableArrayList(listReservations));
+
+        }catch(Exception e){
+            throw new Exception();
+        }
 
         //Mouse Click Event
         ReservationListTable.setOnMouseClicked(event -> {
             if (event.getButton() == MouseButton.SECONDARY) {
-                ReservationModel mdl = ((ReservationModel) ReservationListTable.getSelectionModel().getSelectedItem());
-                HelloApplication.ConfirmationAlert("Confirmation", "Confirmer la réservation", String.format("%s %s a effectué une demande de reservation\ndu véhicule sous immatricule %s\ndu %s à %s ", mdl.Prenom, mdl.Nom, mdl.Immatricule, mdl.DateDepart, mdl.DateRetour));
+                Object mdl = ReservationListTable.getSelectionModel().getSelectedItem();
+
+                StatsController.Rent reservationSelectionnee = (StatsController.Rent) mdl;
+                if (HelloApplication.ConfirmationAlert("Confirmation", "Confirmé cette reservation ?", "Voulez-vous confirmé cette reservations du client : "+ reservationSelectionnee.getNomComplet()+" ?")) {
+                    // Comfirmer la reservation du client :
+
+
+                    HelloApplication.InformationAlert("Success", "", "Reservation Comfirmé !");
+                }
             }
         });
     }
